5b3fd298e5e8410524d9e6b751aaff2333e93fe1
Dipanwita Ghosh <dipanwita.iub@gmail.com>
2018-09-02 16:54:52 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const RPS = require('./rock-paper-scissors');
+
+const app = express();
+
+const clientPath = `${__dirname}/public/`;
+console.log(`Serving static from ${clientPath}`);
+
+app.use(express.static(clientPath));
+
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+let waitingPlayer = null;
+
+io.on('connection', (sock) => {
+
+  if (waitingPlayer) {
+    new RPS(waitingPlayer, sock);
+    waitingPlayer = null;
+  } else {
+    waitingPlayer = sock;
+    waitingPlayer.emit('message', 'Waiting for an opponent');
+  }
+
+  sock.on('message', (text) => {
+    io.emit('message', text);
+  });
+});
+
+server.on('error', (err) => {
+  console.error('Server error:', err);
+});
+
+server.listen(process.env.PORT, process.env.IP, () => {
+  console.log('Server started on 8080');
+});
